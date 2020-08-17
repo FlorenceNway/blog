d8ea9490ded26b32c55e741538a859f8629fe31f
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-17 23:45:26 +0100

    route the components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import store from './store/store'
 import * as serviceWorker from './serviceWorker';
+import BlogList from './pages/BlogList';
+import BlogNew from './pages/BlogNew';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route path="/blog/new" component={BlogNew} />
+        <Route path="/" component={BlogList} />
+      </Switch>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
