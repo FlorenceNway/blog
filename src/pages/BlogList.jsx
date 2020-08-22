742a0bfd5350d2462b00aa0dcdc240a61359c776
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-22 11:35:49 +0100

    history push

--- src/pages/BlogList.jsx
+++ src/pages/BlogList.jsx
@@ -4,7 +4,10 @@ import { Link } from 'react-router-dom'
 import {connect} from 'react-redux';
 import {getAllBlogs} from '../services/blogAction';
 import _ from 'lodash';
-
+import { Button, Grid, Table } from "semantic-ui-react";
+import styles from './blogList.module.scss';
+import classNames from 'classnames';
+import CustomButton from "./components/CustomButton";
 
 class BlogList extends Component {
 
@@ -13,40 +16,49 @@ class BlogList extends Component {
     }
 
     render() {
-        //console.log(this.props.blogs)
+        console.log(this.props.blogs)
         const {blogs} = this.props
-        return (
-          <div className="p-5">
-            <div className="text-right">
-              <Link to="/blog/new" className="btn btn-primary">
-                New blog
-              </Link>
-            </div>
 
-            <div>
-              <h3>Blog List</h3>
-              <table className="table table-hover">
-                <thead>
-                  <tr>
-                    <th>Title</th>
-                    <th>Categories</th>
-                    <th>Content</th>
-                  </tr>
-                </thead>
-                <tbody>
-                    {
-                        _.map(blogs,(blog, index) => {
-                            return <tr key={blog.id}>
-                                    <td>{index + 1}</td>
-                                    <td>{blog?.title}</td>
-                                    <td>{blog?.categories}</td>
-                                    <td>{blog?.content}</td>
-                                </tr>
-                        })
-                    }
-                </tbody>
-              </table>
-            </div>
+        return (
+          <div className={classNames(styles.layout, styles.layoutPadding)}>
+            <Grid padded>
+              <Grid.Row>
+                <Grid.Column>
+                  <CustomButton {...this.props}/>
+                  {/* <Link to="/blog/new">
+                    <Button color="orange">New blog</Button>
+                  </Link> */}
+                </Grid.Column>
+              </Grid.Row>
+
+              <Grid.Row>
+                <Grid.Column>
+                  <h3>Blog List</h3>
+                  <Table>
+                    <Table.Header>
+                      <Table.Row>
+                        <Table.HeaderCell>#</Table.HeaderCell>
+                        <Table.HeaderCell>Title</Table.HeaderCell>
+                        <Table.HeaderCell>Categories</Table.HeaderCell>
+                        <Table.HeaderCell>Content</Table.HeaderCell>
+                      </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                      {_.map(blogs, (blog, index) => {
+                        return (
+                          <Table.Row key={blog.id}>
+                            <Table.Cell>{index + 1}</Table.Cell>
+                            <Table.Cell>{blog?.title}</Table.Cell>
+                            <Table.Cell>{blog?.categories}</Table.Cell>
+                            <Table.Cell>{blog?.content}</Table.Cell>
+                          </Table.Row>
+                        );
+                      })}
+                    </Table.Body>
+                  </Table>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
           </div>
         );
     }
