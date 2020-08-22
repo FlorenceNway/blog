8b48b1c78f55fe3a99166d39213d3770c2be0a0b
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-22 11:47:22 +0100

    use redux Connected router for history.push

--- src/index.js
+++ src/index.js
@@ -3,19 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {Provider} from 'react-redux';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {ConnectedRouter} from 'connected-react-router';
 import store from './store/store'
 import * as serviceWorker from './serviceWorker';
 import BlogList from './pages/BlogList';
 import BlogNew from './pages/BlogNew';
+import {history} from './store/store.jsx'
+import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <Switch>
-        <Route path="/blog/new" component={BlogNew} />
-        <Route path="/" component={BlogList} />
-      </Switch>
-    </Router>
+    <ConnectedRouter history={history}>
+      <Router>
+        <Switch>
+          <Route path="/blog/new" component={BlogNew} />
+          <Route path="/" component={BlogList} />
+        </Switch>
+      </Router>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
 );
