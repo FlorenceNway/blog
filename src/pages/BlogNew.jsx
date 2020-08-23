89bbb54e7c8fbaceb6435fa128fa2b701c4356d8
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-23 02:01:10 +0100

    use Layout component

--- src/pages/BlogNew.jsx
+++ src/pages/BlogNew.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import Layout from "../components/Layout";
+
 
 const BlogNew = () => {
-  return <div>BlogNew</div>;
+  return <Layout header="New Blog">Blog New</Layout>;
 };
 export default BlogNew;
