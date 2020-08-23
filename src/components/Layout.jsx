e830f04b1ace6dbe8c0cd15ca1339ab49ba318d8
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-23 10:48:01 +0100

    coment out

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import { Header } from "semantic-ui-react";
 import styles from './layout.module.scss'
 
 
- const Layout = ({children,header}) => {
+ const Layout = ({children,header}) => { //{children, header, ...rest}
     return (
       <div className={classNames(styles.layout, styles.layoutPadding)}>
         <Header as="h1">{header}</Header>
