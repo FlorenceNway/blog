64db041d407521157df864fefe72c0b51ce6d51c
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-23 11:16:47 +0100

    include proptypes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import classNames from 'classnames'
-import { Header } from "semantic-ui-react";
-import styles from './layout.module.scss'
+import React from 'react';
+import propTypes from 'prop-types';
+import classNames from 'classnames';
+import { Header } from 'semantic-ui-react';
+import styles from './layout.module.scss';
 
+const Layout = ({ children, header }) => {
+  // {children, header, ...rest}
+  return (
+    <div className={classNames(styles.layout, styles.layoutPadding)}>
+      <Header as="h1">{header}</Header>
+      {children}
+    </div>
+  );
+};
 
- const Layout = ({children,header}) => { //{children, header, ...rest}
-    return (
-      <div className={classNames(styles.layout, styles.layoutPadding)}>
-        <Header as="h1">{header}</Header>
-        {children}
-      </div>
-    );
-}
-export default Layout
+Layout.propTypes = {
+  header: propTypes.string,
+};
+
+Layout.defaultProps = {
+  header: '',
+};
+
+export default Layout;
