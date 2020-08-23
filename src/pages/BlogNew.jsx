bb2560824f158e2522c4e245d05316500644991b
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-23 11:51:14 +0100

    handleSubmit from redux is used to get input data

--- src/pages/BlogNew.jsx
+++ src/pages/BlogNew.jsx
@@ -1,11 +1,38 @@
-import React from "react";
-import Layout from "../components/Layout";
+import React from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { Button, Grid, Segment, Form } from 'semantic-ui-react';
+import Layout from '../components/Layout';
 
+const BlogNew = (props) => {
+  const { handleSubmit } = props;
+  // console.log('prop',props)
 
-const BlogNew = () => {
-  return <Layout header="New Blog">
-              <h3>Blog New</h3>
-              
-          </Layout>;
+  const onFormSubmit = (data) => {
+    console.log('here is data =>', data)
+  }
+
+  return (
+    <Layout header="New Blog">
+      <Grid>
+        <Grid.Row>
+          <Grid.Column computer={8} tablet={16} mobile={16}>
+            <Segment padded>
+              <Form onSubmit={handleSubmit(onFormSubmit)}>
+                <Form.Field>
+                  <label>Name</label>
+                  <Field name="name" component="input" type="text" />
+                </Form.Field>
+                <Form.Field>
+                  <label>Blog</label>
+                  <Field name="blog" component="input" type="text" />
+                </Form.Field>
+                <Button primary>Save</Button>
+              </Form>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Layout>
+  );
 };
-export default BlogNew;
+export default reduxForm({ form: 'blogNewForm' })(BlogNew);
