fbf33dd5c77ff69bf1aa9f3b2c0edea73dc3013c
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-23 02:01:30 +0100

    connected router use for history

--- src/pages/components/CustomButton.jsx
+++ src/pages/components/CustomButton.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import {Button} from "semantic-ui-react"
+import {connect} from "react-redux"
+import {push} from "../../services/router/routerAction"
 
  const CustomButton = (props) => {
     return (
       <div>
-        <Button color="orange"
-            onClick={() => props.history.push("./blog/new") }
-        >New blog
+        <Button color="orange" onClick={() => props.push("./blog/new") }>
+            New blog
         </Button>
       </div>
     );
 }
-export default CustomButton
+export default connect(null, {push})(CustomButton)
