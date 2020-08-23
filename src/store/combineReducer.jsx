8561e37abd60520dc4c174409b4cfcbf0a4ba46e
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-23 11:51:25 +0100

    conbine form reducer

--- src/store/combineReducer.jsx
+++ src/store/combineReducer.jsx
@@ -1,9 +1,12 @@
-import {combineReducers} from 'redux';
-import {connectRouter} from "connected-react-router"
-import blogReducer from '../services/blogReducer'
+import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
+import { reducer as form } from 'redux-form';
+// import { reducer } from 'redux-form';
+import blogReducer from '../services/blogReducer';
 
-export default (history) => combineReducers({
-  router: connectRouter(history),
-  blogReducer,
-});
-
+export default (history) =>
+  combineReducers({
+    router: connectRouter(history),
+    blogReducer,
+    form, // form: reducer,
+  });
