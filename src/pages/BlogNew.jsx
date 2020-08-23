d11aec3658fdb8c6ad23bda9f4aee3ab5b867e5b
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-23 10:48:41 +0100

    add h3

--- src/pages/BlogNew.jsx
+++ src/pages/BlogNew.jsx
@@ -3,6 +3,9 @@ import Layout from "../components/Layout";
 
 
 const BlogNew = () => {
-  return <Layout header="New Blog">Blog New</Layout>;
+  return <Layout header="New Blog">
+              <h3>Blog New</h3>
+              
+          </Layout>;
 };
 export default BlogNew;
