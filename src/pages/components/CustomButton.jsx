a0702340a833f63d78fa8df02cc78877345f0da8
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-26 00:02:39 +0100

    fix eslint errors

--- src/pages/components/CustomButton.jsx
+++ src/pages/components/CustomButton.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import {Button} from "semantic-ui-react"
-import {connect} from "react-redux"
-import {push} from "../../services/router/routerAction"
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { push } from '../../services/router/routerAction';
 
- const CustomButton = (props) => {
-    return (
-      <div>
-        <Button color="orange" onClick={() => props.push("./blog/new") }>
-            New blog
-        </Button>
-      </div>
-    );
-}
-export default connect(null, {push})(CustomButton)
+const CustomButton = (props) => {
+  return (
+    <div>
+      <Button color="orange" onClick={() => props.push('./blog/new')}>
+        New blog
+      </Button>
+    </div>
+  );
+};
+export default connect(null, { push })(CustomButton);
