c35a5fedb2fea38082b2b0e3ca4c77b0f03adc9a
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-26 00:03:58 +0100

    eslint error fix

--- src/services/router/routerAction.jsx
+++ src/services/router/routerAction.jsx
@@ -2,7 +2,7 @@ import {
   push as routerPush, //wanna use push as function name, thats why rename it
   goBack as routerGoBack,
   replace as routerReplace,
-} from "connected-react-router";
+} from 'connected-react-router';
 
 export const push = (path) => (dispatch) => {
   dispatch(routerPush(path));
