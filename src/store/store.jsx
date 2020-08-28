db73cbb6f2dacfdc0f94f52cb39eac78e5f3b1f0
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-28 23:27:36 +0100

    eslint error fix

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,10 +1,10 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 // import { persistStore, persistReducer } from 'redux-persist';
 // import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-import { createBrowserHistory } from "history";
-import { routerMiddleware } from "connected-react-router";
-import reducers from "./combineReducer";
+import { createBrowserHistory } from 'history';
+import { routerMiddleware } from 'connected-react-router';
+import reducers from './combineReducer';
 
 // const persistConfig = {
 //   key: 'root',
@@ -16,27 +16,27 @@ export const history = createBrowserHistory();
 // const persistedReducer = persistReducer(persistConfig, reducers(history));
 
 const middlewares = [thunk];
-if (process.env.NODE_ENV === "development") {
-    const { createLogger } = require("redux-logger"); // eslint-disable-line global-require
+if (process.env.NODE_ENV === 'development') {
+  const { createLogger } = require('redux-logger'); // eslint-disable-line global-require
 
-    const logger = createLogger({
-        level: "log",
-        collapsed: true,
-        // predicate: (getState, action) => !includes(action.type, '@@redux-form')
-    });
-    middlewares.push(logger);
+  const logger = createLogger({
+    level: 'log',
+    collapsed: true,
+    // predicate: (getState, action) => !includes(action.type, '@@redux-form')
+  });
+  middlewares.push(logger);
 }
 
 const createStoreWithMiddleware = applyMiddleware(
-    routerMiddleware(history),
-    ...middlewares
+  routerMiddleware(history),
+  ...middlewares
 )(createStore);
 
-//eslint-disable no-underscore-dangle //
 // export const store = createStoreWithMiddleware(persistedReducer);
 const store = createStoreWithMiddleware(
-    reducers(history),
-    window.__REDUX_DEVTOOLS_EXTENSION_ && window.__REDUX_DEVTOOLS_EXTENSION_()
+  reducers(history),
+  // eslint-disable-next-line no-underscore-dangle
+  window.__REDUX_DEVTOOLS_EXTENSION_ && window.__REDUX_DEVTOOLS_EXTENSION_()
 );
 // eslint-enable //
 
