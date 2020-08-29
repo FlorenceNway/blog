ff4b9207c4b018ea22443bdcbbf422eaf0ff6e1c
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-29 12:01:53 +0100

    reset button added

--- src/pages/BlogNew.jsx
+++ src/pages/BlogNew.jsx
@@ -1,17 +1,23 @@
 /* eslint-disable no-console */
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { reduxForm } from 'redux-form';
 import { Button, Grid, Segment, Form } from 'semantic-ui-react';
 import Layout from '../components/Layout';
 import BackButton from './components/BackButton';
 import Field from './components/fields/Field';
+import { createBlog } from '../services/blogAction';
+import styles from './blogForm.module.scss';
 
 const BlogNew = (props) => {
+  const dispatch = useDispatch();
   const { handleSubmit, pristine, submitting } = props;
   // console.log('prop',props)
 
   const onFormSubmit = (data) => {
-    console.log('here is data =>', data);
+    // console.log('here is data =>', data);
+    dispatch(createBlog(data));
+    props.reset(); // from redux form
   };
 
   return (
@@ -24,11 +30,18 @@ const BlogNew = (props) => {
                 <Field label="Name" name="name" isRequired />
                 <Field label="Title" name="title" isRequired />
                 <Field label="Description" name="description" />
-                <Button primary disabled={pristine || submitting}>
-                  {/* use redux Form, only if it has content, the buttons will be enable */}
-                  Save
-                </Button>
-                <BackButton secondary />
+                <div className={styles.buttonRow}>
+                  <BackButton secondary />
+                  <div>
+                    <Button primary disabled={pristine || submitting}>
+                      {/* use redux Form, only if it has content, the buttons will be enable */}
+                      Save
+                    </Button>
+                    <Button color="red" onClick={() => props.reset()}>
+                      Reset
+                    </Button>
+                  </div>
+                </div>
               </Form>
             </Segment>
           </Grid.Column>
