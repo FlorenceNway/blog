1605515ea9add5001c78c485046a001940ce41b4
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-29 12:02:12 +0100

    change using with dispatch

--- src/pages/components/BackButton.jsx
+++ src/pages/components/BackButton.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Button } from 'semantic-ui-react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import { goBack } from '../../services/router/routerAction';
 
-const BackButton = (props) => {
+// const BackButton = (props) => {
+//   return (
+//     <Button color="grey" onClick={() => props.goBack()}>
+//       Back
+//     </Button>
+//   );
+// };
+// export default connect(null, { goBack })(BackButton);
+
+const BackButton = () => {
+  const dispatch = useDispatch();
+
   return (
-    <Button color="grey" onClick={() => props.goBack()}>
+    <Button color="grey" onClick={() => dispatch(goBack())}>
       Back
     </Button>
   );
 };
-export default connect(null, { goBack })(BackButton);
+
+export default BackButton;
