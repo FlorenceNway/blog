db87901ef8c99b8872a977526fd54248f8184cdd
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-30 10:50:37 +0100

    buttons added for delete and edit

--- src/pages/BlogList.jsx
+++ src/pages/BlogList.jsx
@@ -2,8 +2,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-import { Grid, Table } from 'semantic-ui-react';
-import { getAllBlogs } from '../services/blogAction';
+import { Button, Grid, Table, TableCell } from 'semantic-ui-react';
+import { getAllBlogs, deleteBlog } from '../services/blogAction';
 import Layout from '../components/Layout';
 import CreateButton from './components/CreateButton';
 
@@ -39,16 +39,29 @@ class BlogList extends Component {
                     <Table.HeaderCell>Name</Table.HeaderCell>
                     <Table.HeaderCell>Title</Table.HeaderCell>
                     <Table.HeaderCell>Description</Table.HeaderCell>
+                    <Table.HeaderCell>Action</Table.HeaderCell>
                   </Table.Row>
                 </Table.Header>
                 <Table.Body>
                   {_.map(blogs, (blog, index) => {
                     return (
-                      <Table.Row key={blog.id}>
+                      <Table.Row key={index}>
                         <Table.Cell>{index}</Table.Cell>
                         <Table.Cell>{blog?.name}</Table.Cell>
                         <Table.Cell>{blog?.title}</Table.Cell>
                         <Table.Cell>{blog?.description}</Table.Cell>
+                        <TableCell>
+                          <Button
+                            color="red"
+                            attached="left"
+                            onClick={() => this.props.deleteBlog(index)}
+                          >
+                            DELETE
+                          </Button>
+                          <Button color="green" attached="right">
+                            Edit
+                          </Button>
+                        </TableCell>
                       </Table.Row>
                     );
                   })}
@@ -64,5 +77,5 @@ class BlogList extends Component {
 const mapStateToProps = ({ blogReducer }) => ({
   blogs: blogReducer.blogs,
 });
-export default connect(mapStateToProps, { getAllBlogs })(BlogList);
+export default connect(mapStateToProps, { getAllBlogs, deleteBlog })(BlogList);
 // connect(state,action)(component)
