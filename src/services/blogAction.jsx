c3f3e2fd8f5cb325b10bde3d0c34f1e44813b74b
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-30 10:53:58 +0100

    delete action

--- src/services/blogAction.jsx
+++ src/services/blogAction.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
-import { GET_BLOGS, CREATE_BLOG } from './blogActionTypes';
+import { GET_BLOGS, CREATE_BLOG, DELETE_BLOG } from './blogActionTypes';
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api/posts';
+// const ROOT_URL = 'http://reduxblog.herokuapp.com/api/posts'; // table name = blog, firebase needs json format
+const url = 'https://blogs-34bb0.firebaseio.com/blog.json';
 
 // export const getAllBlogs = () => (dispatch) => {
 //     axios.get(ROOT_URL).then((response) => {
@@ -14,7 +15,7 @@ const ROOT_URL = 'http://reduxblog.herokuapp.com/api/posts';
 // }
 
 export const getAllBlogs = () => async (dispatch) => {
-  const response = await axios.get(ROOT_URL);
+  const response = await axios.get(url);
   return dispatch({
     type: GET_BLOGS,
     payload: response.data,
@@ -22,10 +23,21 @@ export const getAllBlogs = () => async (dispatch) => {
 };
 
 export const createBlog = (data) => async (dispatch) => {
-  const url = 'https://blogs-34bb0.firebaseio.com/blog.json'; // table name - blog, firebase needs json format
   const response = await axios.post(url, data);
   return dispatch({
     type: CREATE_BLOG,
+    payload: response.data,
+  });
+};
+
+export const deleteBlog = (id, onSuccess) => async (dispatch) => {
+  const delUrl = `https://blogs-34bb0.firebaseio.com/blog/${id}.json`;
+  const response = await axios.delete(delUrl);
+  if (onSuccess) {
+    onSuccess();
+  }
+  return dispatch({
+    type: DELETE_BLOG,
     payload: response.data,
   });
 };
