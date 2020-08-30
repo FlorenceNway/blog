1a8d7490c277208bf667719048059a87a49ff488
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-30 11:30:50 +0100

    del modal

---
+++ src/components/deleteModal/DeleteModal.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Modal, Header, Button } from 'semantic-ui-react';
+
+const DeleteModal = (props) => {
+  const { isOpen, onClose } = props;
+  return (
+    <Modal onClose={onClose} isOpen={isOpen} size="mini">
+      <Modal.Content>
+        <Modal.Description>
+          <Header>Delete</Header>
+          <p>Are you sure? Do you want to delete?</p>
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color="red">No</Button>
+        <Button color="green">Yes</Button>
+      </Modal.Actions>
+    </Modal>
+  );
+};
+export default DeleteModal;
