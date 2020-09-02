362716a8c5ff116d5b031068b9fbe86cb25224e9
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-09-02 14:22:49 +0100

    go back upon saving new blog

--- src/pages/BlogNew.jsx
+++ src/pages/BlogNew.jsx
@@ -2,21 +2,27 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { reduxForm } from 'redux-form';
-import { Button, Grid, Segment, Form } from 'semantic-ui-react';
+import { Grid, Segment } from 'semantic-ui-react';
 import Layout from '../components/Layout';
-import BackButton from './components/BackButton';
-import Field from './components/fields/Field';
+// import BackButton from './components/BackButton';
+// import Field from './components/fields/Field';
 import { createBlog } from '../services/blogAction';
-import styles from './blogForm.module.scss';
+// import styles from './blogForm.module.scss';
+import BlogForm from './components/BlogForm';
+import { goBack } from '../services/router/routerAction';
 
 const BlogNew = (props) => {
   const dispatch = useDispatch();
-  const { handleSubmit, pristine, submitting } = props;
+  // const { handleSubmit, pristine, submitting } = props;
   // console.log('prop',props)
 
   const onFormSubmit = (data) => {
     // console.log('here is data =>', data);
-    dispatch(createBlog(data));
+    dispatch(
+      createBlog(data, () => {
+        dispatch(goBack());
+      })
+    );
     props.reset(); // from redux form
   };
 
@@ -26,23 +32,7 @@ const BlogNew = (props) => {
         <Grid.Row>
           <Grid.Column computer={8} tablet={16} mobile={16}>
             <Segment padded>
-              <Form onSubmit={handleSubmit(onFormSubmit)}>
-                <Field label="Name" name="name" isRequired />
-                <Field label="Title" name="title" isRequired />
-                <Field label="Description" name="description" />
-                <div className={styles.buttonRow}>
-                  <BackButton secondary />
-                  <div>
-                    <Button primary disabled={pristine || submitting}>
-                      {/* use redux Form, only if it has content, the buttons will be enable */}
-                      Save
-                    </Button>
-                    <Button color="red" onClick={() => props.reset()}>
-                      Reset
-                    </Button>
-                  </div>
-                </div>
-              </Form>
+              <BlogForm {...props} onFormSubmit={onFormSubmit} />
             </Segment>
           </Grid.Column>
         </Grid.Row>
