3da0bc6288eb562783ec0b6db4180a61b0a297a8
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-09-13 14:38:11 +0100

    upldate blog action

--- src/services/blogActionTypes.jsx
+++ src/services/blogActionTypes.jsx
@@ -2,3 +2,4 @@ export const GET_BLOGS = 'GET_BLOGS';
 export const CREATE_BLOG = 'CREATE_BLOG';
 export const DELETE_BLOG = 'DELETE_BLOG';
 export const GET_ONE_BLOG = 'GET_ONE_BLOG';
+export const UPDATE_BLOG = 'UPDATE_BLOG';
