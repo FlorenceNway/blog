fe5fbdd6077333ad2b80c9bb24523b7f81b6f41a
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-09-13 15:06:46 +0100

    isPending state added

--- src/services/blogReducer.jsx
+++ src/services/blogReducer.jsx
@@ -1,10 +1,16 @@
-import { GET_BLOGS } from './blogActionTypes';
+import { GET_BLOGS, GET_ONE_BLOG } from './blogActionTypes';
 
-const initialState = { blogs: [] };
+const initialState = { blogs: [], isPending: false, error: false };
 
 export default function (state = { initialState }, action) {
   switch (action.type) {
     case GET_BLOGS:
+      return {
+        ...state,
+        isPending: action.meta.isPending,
+        blogs: action.payload,
+      };
+    case GET_ONE_BLOG:
       return {
         ...state,
         blogs: action.payload,
