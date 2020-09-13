653569a25be676f713c5aed6c06fa9222544934e
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-09-13 14:33:21 +0100

    reset and go back after submit

--- src/pages/BlogUpdate.jsx
+++ src/pages/BlogUpdate.jsx
@@ -4,21 +4,24 @@ import { reduxForm } from 'redux-form';
 import { Grid, Segment } from 'semantic-ui-react';
 import Layout from '../components/Layout';
 import BlogForm from './components/BlogForm';
-import { getOneBlog } from '../services/blogAction';
+import { getOneBlog, updateBlog } from '../services/blogAction';
+import { goBack } from '../services/router/routerAction';
 
 const BlogUpdate = (props) => {
   const { getOneBlog: getBlog } = props;
+  const { id } = props.match.params;
 
   useEffect(() => {
-    const { id } = props.match.params;
     // console.log('i', id);
     getBlog(id);
   }, [getBlog]);
 
-  const onFormSubmit = () => {};
+  const onFormSubmit = (data) => {
+    props.updateBlog(id, data, props.goBack());
+  };
 
   return (
-    <Layout header="New Blog">
+    <Layout header="Update Blog">
       <Grid>
         <Grid.Row>
           <Grid.Column computer={8} tablet={16} mobile={16}>
@@ -31,6 +34,14 @@ const BlogUpdate = (props) => {
     </Layout>
   );
 };
-export default connect(null, { getOneBlog })(
-  reduxForm({ form: 'updateForm' })(BlogUpdate)
+
+const mapStateToProps = ({ blogReducer }) => {
+  // console.log(blogReducer);
+  return {
+    initialValues: blogReducer?.blogs,
+  };
+};
+
+export default connect(mapStateToProps, { getOneBlog, updateBlog, goBack })(
+  reduxForm({ form: 'updateForm', enableReinitialize: true })(BlogUpdate)
 );
