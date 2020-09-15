cb61002298a3edf87b8715b2f281b7b923832552
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-09-15 11:08:28 +0100

    disable buttons when it is pending

--- src/pages/components/BlogForm.jsx
+++ src/pages/components/BlogForm.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Button, Form } from 'semantic-ui-react';
 import Field from './fields/Field';
 import styles from '../blogForm.module.scss';
 import BackButton from './BackButton';
 
 const BlogForm = (props) => {
+  const blogReducer = useSelector((state) => state.blogReducer);
+  const isPending = blogReducer.isPending;
   const { handleSubmit, onFormSubmit, pristine, submitting, reset } = props;
-
+  console.log('pending', isPending);
   return (
     <Form onSubmit={handleSubmit(onFormSubmit)}>
       <Field label="Name" name="name" isRequired />
@@ -16,7 +19,11 @@ const BlogForm = (props) => {
         <BackButton secondary />
 
         <div>
-          <Button primary disabled={pristine || submitting}>
+          <Button
+            primary
+            disabled={pristine || submitting || isPending}
+            loading={isPending}
+          >
             {/* use redux Form, only if it has content, the buttons will be enable */}
             Save
           </Button>
