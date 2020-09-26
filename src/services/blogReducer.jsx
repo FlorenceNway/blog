71b5c1e3359b66c025eb639f524293d2e4fd31a8
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-09-26 02:28:44 +0100

    check isPending

--- src/services/blogReducer.jsx
+++ src/services/blogReducer.jsx
@@ -13,7 +13,7 @@ export default function (state = { initialState }, action) {
     case GET_BLOGS:
       return {
         ...state,
-        isPending: action.meta.isPending,
+        isPending: action?.meta?.isPending,
         blogs: action.payload,
       };
     case GET_ONE_BLOG:
